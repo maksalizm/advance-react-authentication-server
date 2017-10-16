de0f58533937d0220a69606d1c2590caa9fcb1a3
maksalizm <maksalizm@gmail.com>
2017-10-17 06:22:00 +0900

    first commit

---
+++ controllers/authentication.js
@@ -0,0 +1,18 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+    const { email, password } = req.body;
+
+    // See if a user with the given email exists
+    User.findOne({ email }, (err, existingUser) => {
+        
+    })
+
+
+    // If a user with email does exit, return an error
+
+    // If a user with email does NOT exist, create and save user record
+
+    // Respond to request indicating the user was created
+
+};
